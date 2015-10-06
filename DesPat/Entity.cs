c823abfcd22284f930fbd64fbf3c842dc0263674
Nottycat <Nottykittten@gmail.com>
2015-10-06 10:38:54 +0200

    Start Entity class
    Entity class added and implemented.

---
+++ DesPat/Entity.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesPat
+{
+    public class Entity
+    {
+        protected TextureObj textureObj;
+        protected Input entityInput;
+
+        protected float movementSpeed;
+        protected float rotateSpeed;
+
+        protected void checkInput()
+        {
+            entityInput.update();
+            executeMovement(entityInput.up, entityInput.left, entityInput.down, entityInput.right);
+        }
+        private void executeMovement(bool up, bool left, bool down, bool right)
+        {
+            var movementDelta = Vector2.Zero;
+            float angle = textureObj.getAngle();
+            if (up)
+            {
+                if (angle >= 0 && angle < 90)
+                {
+                    movementDelta.X += movementSpeed * ((angle) / 45);
+                    movementDelta.Y -= movementSpeed * ((2f - (angle) / 45));
+                }
+                else if (angle >= 90 && angle < 180)
+                {
+                    movementDelta.X += movementSpeed * ((2f - ((90 - (180 - angle)) / 45)));
+                    movementDelta.Y += movementSpeed * ((90 - (180 - angle)) / 45);
+                }
+                else if (angle >= 180 && angle < 270)
+                {
+                    movementDelta.X -= movementSpeed * ((90 - (270 - angle)) / 45);
+                    movementDelta.Y += movementSpeed * (2f - ((90 - (270 - angle)) / 45));
+                }
+                else if (angle >= 270 && angle < 360)
+                {
+                    movementDelta.X -= movementSpeed * ((360 - angle) / 45);
+                    movementDelta.Y -= movementSpeed * (2f - ((360 - angle) / 45));
+                }
+            }
+            if (left)
+            {
+                angle -= rotateSpeed;
+            }
+            if (down)
+            {
+                if (angle >= 0 && angle < 90)
+                {
+                    movementDelta.X -= movementSpeed * ((angle) / 45);
+                    movementDelta.Y += movementSpeed * ((2f - (angle) / 45));
+                }
+                else if (angle >= 90 && angle < 180)
+                {
+                    movementDelta.X -= movementSpeed * ((2f - ((90 - (180 - angle)) / 45)));
+                    movementDelta.Y -= movementSpeed * ((90 - (180 - angle)) / 45);
+                }
+                else if (angle >= 180 && angle < 270)
+                {
+                    movementDelta.X += movementSpeed * ((90 - (270 - angle)) / 45);
+                    movementDelta.Y -= movementSpeed * (2f - ((90 - (270 - angle)) / 45));
+                }
+                else if (angle >= 270 && angle < 360)
+                {
+                    movementDelta.X += movementSpeed * ((360 - angle) / 45);
+                    movementDelta.Y += movementSpeed * (2f - ((360 - angle) / 45));
+                }
+            }
+            if (right)
+            {
+                angle += rotateSpeed;
+            }
+
+            textureObj.changeAngle(angle);
+            textureObj.addToLocation(movementDelta);
+
+            if (textureObj.getLocation().X <= textureObj.getTexture().Width / 2)
+                textureObj.changeLocation(textureObj.getTexture().Width / 2, textureObj.getLocation().Y);
+            if (textureObj.getLocation().X + textureObj.getTexture().Width / 2 >= Main.screenWidth)
+                textureObj.changeLocation(Main.screenWidth - textureObj.getTexture().Width / 2, textureObj.getLocation().Y);
+
+            if (textureObj.getLocation().Y <= textureObj.getTexture().Height / 2)
+                textureObj.changeLocation(textureObj.getLocation().X, textureObj.getTexture().Height / 2);
+            if (textureObj.getLocation().Y + textureObj.getTexture().Height / 2 >= Main.screenHeight)
+                textureObj.changeLocation(textureObj.getLocation().X, Main.screenHeight - textureObj.getTexture().Height / 2);
+        }
+    }
+}
--- DesPat/Player.cs
+++ DesPat/Player.cs
@@ -8,15 +8,9 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace DesPat
 {
-    public class Player
+    public class Player : Entity
     {
         private int playerNumber;
-        private TextureObj textureObj;
-
-        private Input playerInput;
-
-        private float movementSpeed;
-        private float rotateSpeed;
 
         private long lastShot = 0;
 
@@ -31,84 +25,23 @@ namespace DesPat
         public static SharpDX.XInput.Controller controller = new SharpDX.XInput.Controller();
         public static SharpDX.XInput.Vibration vibration = new SharpDX.XInput.Vibration();
 
-        public Player(int playerNumber, TextureObj textureObj, float movementSpeed, float rotateSpeed, Vector2 healthBarLocation, Input playerInput)
+        public Player(int playerNumber, TextureObj textureObj, float movementSpeed, float rotateSpeed, Vector2 healthBarLocation, Input entityInput)
         {
             this.playerNumber = playerNumber;
-            this.textureObj = textureObj;
-            this.movementSpeed = movementSpeed;
-            this.rotateSpeed = rotateSpeed;
+            base.textureObj = textureObj;
+            base.movementSpeed = movementSpeed;
+            base.rotateSpeed = rotateSpeed;
             this.healthBarLocation = healthBarLocation;
-            this.playerInput = playerInput;
+            base.entityInput = entityInput;
             
             String objName = "Life-" + (int)((health / maxHealth) * 3) + ".png";
             Texture2D healthBar = Main.imageList.Find(obj => obj.Name == objName);
             Main.addAsActive(new TextureObj(playerNumber, healthBar, healthBarLocation, new Rectangle(0, 0, healthBar.Width, healthBar.Height), Color.White, 0, new Vector2(healthBar.Width / 2, healthBar.Height / 2), 1.0f, SpriteEffects.None, 1, "Healthbar"));
         }
-
-        public void checkInput()
+        public new void checkInput()
         {
-            playerInput.update();
-            executeKeys(playerInput.up, playerInput.left, playerInput.down, playerInput.right, playerInput.shooting);        
-        }
-        private void executeKeys(bool up, bool left, bool down, bool right, bool shoot)
-        {
-            var movementDelta = Vector2.Zero;
-            float angle = textureObj.getAngle();
-            if (up)
-            {
-                if (angle >= 0 && angle < 90)
-                {
-                    movementDelta.X += movementSpeed * ((angle) / 45);
-                    movementDelta.Y -= movementSpeed * ((2f - (angle) / 45));
-                }
-                else if (angle >= 90 && angle < 180)
-                {
-                    movementDelta.X += movementSpeed * ((2f - ((90 - (180 - angle)) / 45)));
-                    movementDelta.Y += movementSpeed * ((90 - (180 - angle)) / 45);
-                }
-                else if (angle >= 180 && angle < 270)
-                {
-                    movementDelta.X -= movementSpeed * ((90 - (270 - angle)) / 45);
-                    movementDelta.Y += movementSpeed * (2f - ((90 - (270 - angle)) / 45));
-                }
-                else if (angle >= 270 && angle < 360)
-                {
-                    movementDelta.X -= movementSpeed * ((360 - angle) / 45);
-                    movementDelta.Y -= movementSpeed * (2f - ((360 - angle) / 45));
-                }
-            }
-            if (left)
-            {
-                angle -= rotateSpeed;
-            }
-            if (down)
-            {
-                if (angle >= 0 && angle < 90)
-                {
-                    movementDelta.X -= movementSpeed * ((angle) / 45);
-                    movementDelta.Y += movementSpeed * ((2f - (angle) / 45));
-                }
-                else if (angle >= 90 && angle < 180)
-                {
-                    movementDelta.X -= movementSpeed * ((2f - ((90 - (180 - angle)) / 45)));
-                    movementDelta.Y -= movementSpeed * ((90 - (180 - angle)) / 45);
-                }
-                else if (angle >= 180 && angle < 270)
-                {
-                    movementDelta.X += movementSpeed * ((90 - (270 - angle)) / 45);
-                    movementDelta.Y -= movementSpeed * (2f - ((90 - (270 - angle)) / 45));
-                }
-                else if (angle >= 270 && angle < 360)
-                {
-                    movementDelta.X += movementSpeed * ((360 - angle) / 45);
-                    movementDelta.Y += movementSpeed * (2f - ((360 - angle) / 45));
-                }
-            }
-            if (right)
-            {
-                angle += rotateSpeed;
-            }
-            if (shoot)
+            base.checkInput();
+            if (entityInput.shooting)
             {
                 if (lastShot == 0 || (DateTime.Now.Ticks - lastShot) / 5000000 >= 1)
                 {
@@ -116,27 +49,12 @@ namespace DesPat
                     lastShot = DateTime.Now.Ticks;
                 }
             }
-
-            textureObj.changeAngle(angle);
-            textureObj.addToLocation(movementDelta);
-
-            if (textureObj.getLocation().X <= textureObj.getTexture().Width / 2)
-                textureObj.changeLocation(textureObj.getTexture().Width / 2 ,textureObj.getLocation().Y);
-            if (textureObj.getLocation().X + textureObj.getTexture().Width / 2 >= Main.screenWidth)
-                textureObj.changeLocation(Main.screenWidth - textureObj.getTexture().Width / 2, textureObj.getLocation().Y);
-
-            if (textureObj.getLocation().Y <= textureObj.getTexture().Height / 2)
-                textureObj.changeLocation(textureObj.getLocation().X, textureObj.getTexture().Height / 2);
-            if (textureObj.getLocation().Y + textureObj.getTexture().Height / 2 >= Main.screenHeight)
-                textureObj.changeLocation(textureObj.getLocation().X, Main.screenHeight - textureObj.getTexture().Height / 2);
-
-
         }
         public void shootProjectile(int playerNumber, double seconds, float speed)
         {
-            vibration.LeftMotorSpeed = leftRumbleMotor;
-            vibration.RightMotorSpeed = rightRumbleMotor;
-            controller.SetVibration(vibration);
+            //vibration.LeftMotorSpeed = leftRumbleMotor;
+            //vibration.RightMotorSpeed = rightRumbleMotor;
+            //controller.SetVibration(vibration);
                
             TextureObj projectileObj = createProjectileObject(playerNumber);
             Main.addAsActive(projectileObj);
